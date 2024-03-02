238c86c58700a748cf645549091d3521e024e3da
Ali Amin <aliamin745@gmail.com>
2024-03-01 23:21:35 -0500

    Vite setup complete

---
+++ front-end/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="w-full h-full min-h-screen flex justify-center items-center"></div>
+	);
+}
+
+export default App;
